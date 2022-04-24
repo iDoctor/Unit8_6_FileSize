9cebebb36b0659f8c23b0a26dc75602e1b52c923
Roman Filatov <ikardoc@gmail.com>
2022-04-24 03:11:57 +0300

    Added FileSizes Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string path = @"c:\Users\Admin\Downloads\30da91a108";
+
+DirectoryInfo di = new DirectoryInfo(path);
+
+long fileSize = 0;
+
+try
+{
+    if (di.Exists)
+    {
+        var allFilesPaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+        foreach (var filePath in allFilesPaths)
+        {
+            if (File.Exists(filePath))
+            {
+                var file = new FileInfo(filePath);
+
+                fileSize += file.Length;
+            }
+            else
+                Console.WriteLine($"Некорректный путь файла '{filePath}'!");
+        }
+
+        Console.WriteLine($"Размер всех файлов в каталоге: {fileSize} b");
+    }
+    else
+        Console.WriteLine("Некорректный путь!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка! {ex.Message}");
+}
